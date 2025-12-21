df809ca922c77265b796c8039e1d768fbb25cd4c
Stephan Fitzpatrick <stephan@knowsuchagency.com>
2025-12-21 05:12:17 +0000

    Fix terminal overflow in task view flex containers
    Add min-h-0 to TaskView and ResizablePanelGroup to complete the
    flexbox fix for nested containers that need explicit min-height: 0.

--- src/routes/tasks/$taskId.tsx
+++ src/routes/tasks/$taskId.tsx
@@ -240,7 +240,7 @@ function TaskView() {
   }
 
   return (
-    <div className="flex h-full flex-col">
+    <div className="flex h-full min-h-0 flex-col">
       {/* Task Header */}
       <div className="flex shrink-0 items-center gap-3 border-b border-border px-4 py-2">
         <div className="flex-1">
@@ -363,7 +363,7 @@ function TaskView() {
       </div>
 
       {/* Split Pane Content */}
-      <ResizablePanelGroup direction="horizontal" className="flex-1">
+      <ResizablePanelGroup direction="horizontal" className="min-h-0 flex-1">
         {/* Left: Terminal */}
         <ResizablePanel defaultSize={50} minSize={30}>
           <TaskTerminal
